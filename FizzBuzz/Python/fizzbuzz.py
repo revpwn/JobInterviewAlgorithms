f1770614a3aa14e36be927d229601f1e51c6d01f
Justin <31374705+JGFM4@users.noreply.github.com>
2018-02-20 16:11:23 -0500

    Fixed range issue
    range in the loop was from 1 to 99. Changed it to 1 to 100.

--- FizzBuzz/Python/fizzbuzz.py
+++ FizzBuzz/Python/fizzbuzz.py
@@ -1,14 +1,14 @@
 # Written by JGFM4
 # Last update: 2018-02-20
 
-for i in (range(1,100)):
-	if (i % 15 == 0):
+for i in range(1,101):
+	if i % 15 == 0:
 		print("FizzBuzz")
 	
-	elif (i % 3 == 0):
+	elif i % 3 == 0:
 		print("Fizz")
 
-	elif (i % 5 == 0):
+	elif i % 5 == 0:
 		print("Buzz")
 
 	else:
