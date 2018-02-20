838bb1aa55cb390a69b42766b9233d948b7818fc
Justin <jmatt081@uottawa.ca>
2018-02-20 14:03:02 -0500

    Added Java solution for FizzBuzz

---
+++ FizzBuzz/Java/FizzBuzz.java
@@ -0,0 +1,22 @@
+// Writen by JGFM4
+// Last update: 2018-02-20
+
+public class FizzBuzz{
+	public static void main(String [] args){
+		
+		for (int i = 1; i <= 100; i++){
+			if (i % 15 == 0){
+				System.out.println("FizzBuzz");
+
+			} else if (i % 3 == 0){
+				System.out.println("Fizz");
+
+			} else if (i % 5 == 0){
+				System.out.println("Buzz");
+
+			} else {
+				System.out.println(i);
+			}
+		}
+	}
+}
