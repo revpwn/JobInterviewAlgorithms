51ad1d6a4637b9f363e35fa280f25c72d26857b5
Austin Tice <austin.tice4@gmail.com>
2018-03-12 00:51:11 -0500

    Rearranging Hierarchy

---
+++ FizzBuzz/Python/fizzbuzz.py
@@ -0,0 +1,12 @@
+# Written by JGFM4
+# Last update: 2018-02-21
+
+for i in range(1,101):
+	if i % 15 == 0:
+		print("FizzBuzz")
+	elif i % 3 == 0:
+		print("Fizz")
+	elif i % 5 == 0:
+		print("Buzz")
+	else:
+		print(i)
