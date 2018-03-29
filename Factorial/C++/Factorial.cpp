c4a67d36f6b623c7850cbedff5ea4455af3fda61
Austin Tice <austin.tice4@gmail.com>
2018-03-29 13:09:05 -0500

    Factorial/C++/Factorial.cpp

--- Factorial/C++/Factorial.cpp
+++ Factorial/C++/Factorial.cpp
@@ -1,8 +1,4 @@
 int factorial(int num){
-	// Default cases - Both 0! & 1! will return 1
-	if(num == 0 || num == 1){
-		return 1;
-	}
 	int total = 1;
 	for(int i = num; i > 1; i--){
 		total *= i;
--- Factorial/Javascript/Factorial.js
+++ Factorial/Javascript/Factorial.js
@@ -1,7 +1,6 @@
-function factorialize(num) {
+function factorial(num) {
   if (num === 0 || num === 1)
     return 1;
   return num * factorialize(num-1);
 }
-
-factorialize(5);
+factorial(5);
