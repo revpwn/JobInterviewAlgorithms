7f3d491eced4a07dc804eaa2feed377c29ec8cc5
Christian Bender <christianbender89@web.de>
2018-04-18 16:59:14 +0200

    I improved the Java AnagramCheck. Made it more compact

--- AnagramCheck/Java/AnagramCheck.java
+++ AnagramCheck/Java/AnagramCheck.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class AnagramCheck {
 
   public static void main(String[] args){
@@ -7,35 +9,15 @@ public class AnagramCheck {
     System.out.println(anagramCheck("Hi", "hello"));
   }
  
- 
   public static boolean anagramCheck(String a, String b){
     //make all letters lower case and remove spaces
     a = a.toLowerCase().replaceAll("\\s","");
     b = b.toLowerCase().replaceAll("\\s","");
-    
-    return insertionSort(a.toCharArray()).equals(insertionSort(b.toCharArray()));
-  }
-  
-  //because the Strings being checked are short, we can use a simpler sorting alg
-  public static String insertionSort(char[] array){
-    int i = 0;
-    while(i < array.length)
-    {
-        int j = i - 1;
-        char x = array[i];
-        while(j >= 0)
-        {
-            if (array[j] >= x)
-                array[j+1] = array[j];
-            else
-                break;
-            j--;
-        }
-        array[j+1] = x;
-        i++;
-    }
-    String result = String.valueOf(array);
-    return result;
+    char aArr[] = a.toCharArray();
+    char bArr[] = b.toCharArray();
+    Arrays.sort(aArr);
+    Arrays.sort(bArr);
+    return Arrays.equals(aArr, bArr);
   }
   
 }
