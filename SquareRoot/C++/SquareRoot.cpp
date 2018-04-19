ba4431ce94fa42bc768488bb7458b8d8c9ebee64
Christian Bender <christianbender89@web.de>
2018-04-19 17:31:56 +0200

    changed the loop instead of a for-loop I put in a while-loop.

--- SquareRoot/C++/SquareRoot.cpp
+++ SquareRoot/C++/SquareRoot.cpp
@@ -2,19 +2,23 @@
 using namespace std;
 
 double sqrt(double);
-int main() {
+int main()
+{
 	// This should output 12
-	cout << sqrt(144) << endl;
+	cout << sqrt(12) << endl;
 	return 0;
 }
 
-double sqrt(double num){
+double sqrt(double num)
+{
 	// r = root
 	// a = change to root
 	double a = 0;
-	double r;
-	for(r = 1.0; ((r*r)-num) > 0.0001 || ((r*r)-num) < -0.0001; r-=a){
-		a = ((r*r)-num)/num;
+	double r = 1.0;
+	while (((r * r) - num) > 0.0001 || ((r * r) - num) < -0.0001)
+	{
+		r -= a;
+		a = ((r * r) - num) / num;
 	}
 	return r;
 }
