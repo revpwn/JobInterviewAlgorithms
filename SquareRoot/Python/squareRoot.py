6d93c9dea3c85677793825a553ef78604b7503cc
Christian Bender <christianbender89@web.de>
2018-04-20 16:43:16 +0200

    changed the formatting

--- SquareRoot/Python/squareRoot.py
+++ SquareRoot/Python/squareRoot.py
@@ -1,6 +1,5 @@
-def square_root(num):
+def squareRoot(num):
     """[summary]
-    Computes the square root.
     Uses the heron-procedure. 
 
     Arguments:
@@ -9,17 +8,15 @@ def square_root(num):
     Returns:
         [float] -- [result]
     """
-
     xn = num / 10.0 # init step
     EPSILON = 0.001 # precision
     while abs(xn**2 - num) > EPSILON:
         xn = 0.5 * (xn + (num/xn))
     return xn
 
-
 def main():
-    print("sqrt(2)={0}".format(square_root(2.0)))
-
+    # Test => 2.000
+    print("sqrt(2)={0}".format(squareRoot(4.0)))
 
 if __name__ == "__main__":
     main()
