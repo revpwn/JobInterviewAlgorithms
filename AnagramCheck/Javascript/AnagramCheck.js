4d7e4708029d9523538486f9bf3cc4fd5a356742
Christian Bender <christianbender89@web.de>
2018-05-21 16:59:57 +0200

    correct the indentation and added some test cases with output

--- AnagramCheck/Javascript/AnagramCheck.js
+++ AnagramCheck/Javascript/AnagramCheck.js
@@ -1,14 +1,17 @@
-  function anagramCheck(x, y){
-  str  = x.toLowerCase()
-             .replace(/\s/g, "")
-             .split("")
-             .sort()
-             .join("");
+function anagramCheck(x, y){
+  str = x.toLowerCase()
+    .replace(/\s/g, "")
+    .split("")
+    .sort()
+    .join("");
   str2 = y.toLowerCase()
-              .replace(/\s/g, "")
-              .split("")
-              .sort()
-              .join("");
-    return str === str2;
+    .replace(/\s/g, "")
+    .split("")
+    .sort()
+    .join("");
+  return str === str2;
 }
-anagramCheck("Hello World", "dlroW olleH");
+
+console.log(anagramCheck("Hello World", "dlroW olleH")); // => true
+console.log(anagramCheck("Imtkheing'", "I'm the king")); // => true
+console.log(anagramCheck("test", "tesd")); // => false
