a4f4a438ebc2917beafd039b4dce28b512e6c105
Christian Bender <christianbender89@web.de>
2018-05-21 16:52:22 +0200

    small changes

--- AnagramCheck/Python/anagramCheck.py
+++ AnagramCheck/Python/anagramCheck.py
@@ -1,12 +1,11 @@
 def anagramCheck(a, b):
-    assert isinstance(a, str)
-    assert isinstance(b, str)
+    assert isinstance(a, str) and isinstance(b, str)
     a = a.replace(" ", "").lower()
     b = b.replace(" ", "").lower()
     return sorted(a) == sorted(b)
 	
 	
 if __name__ == "__main__":
-	print(anagramCheck("Tom Marvolo Riddle", "I am Lord Voldemort"))
-	print(anagramCheck("Hello", "olleH"))
-	print(anagramCheck("Hi", "hello"))
+	print(anagramCheck("Tom Marvolo Riddle", "I am Lord Voldemort")) # => True
+	print(anagramCheck("Hello", "olleH")) # => True
+	print(anagramCheck("Hi", "hello")) # => False
