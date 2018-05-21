343993ae9ed5910f0d866e5ddbc978a58caa3c83
Christian Bender <christianbender89@web.de>
2018-05-21 16:53:33 +0200

    added a main-function

--- AnagramCheck/Python/anagramCheck.py
+++ AnagramCheck/Python/anagramCheck.py
@@ -4,8 +4,10 @@ def anagramCheck(a, b):
     b = b.replace(" ", "").lower()
     return sorted(a) == sorted(b)
 	
-	
+def main():
+    print(anagramCheck("Tom Marvolo Riddle", "I am Lord Voldemort")) # => True
+    print(anagramCheck("Hello", "olleH")) # => True
+    print(anagramCheck("Hi", "hello")) # => False
+
 if __name__ == "__main__":
-	print(anagramCheck("Tom Marvolo Riddle", "I am Lord Voldemort")) # => True
-	print(anagramCheck("Hello", "olleH")) # => True
-	print(anagramCheck("Hi", "hello")) # => False
+	main()
